ce36626fbb1d8a1ba16953cee346832c3e82f641
Jen-Chieh Shen <jcs090218@gmail.com>
2021-11-04 17:28:49 +0800

    Wrap ignore

--- flx-rs.el
+++ flx-rs.el
@@ -6,7 +6,7 @@
 ;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
 ;; Description: flx in Rust using dynamic module
 ;; Keyword: fuzzy
-;; Version: 0.1.0
+;; Version: 0.1.1
 ;; Package-Requires: ((emacs "25.1"))
 ;; URL: https://github.com/jcs-elpa/flx-rs
 
