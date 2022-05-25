227b647e901b2e9282e03b611208e2ca4a417233
Jen-Chieh <jcs090218@gmail.com>
2022-05-25 15:13:16 +0800

    Push new impls

--- core/src/lib.rs
+++ core/src/lib.rs
@@ -19,5 +19,38 @@ emacs::plugin_is_GPL_compatible!();
 // Empty method to satisify emacs module
 #[emacs::module(mod_in_name = false)]
 fn init(_: &Env) -> Result<()> {
+    /// Return the QUERY fuzzy score about STR, using flx fuzzy algorithm.
+    ///
+    /// Sign: (-> Str Str (Option Long))
+    ///
+    /// Return nil if no match happened.
+    ///
+    /// (fn STR QUERY)
+    #[defun]
+    fn score(env: &Env, str: String, query: String) -> Result<Option<Vector>> {
+        let _vec: Option<Vec<i32>> = flx_rs_score(&str, &query);
+        if _vec == None {
+            return Ok(None);
+        }
+        let _inner_vec: Vec<i32> = _vec.unwrap();
+        let mut vec = env.make_vector(_inner_vec.len(), ())?;
+        let mut index = 0;
+        for data in _inner_vec {
+            vec.set(index, data);
+            index += 1;
+        }
+        Ok(Some(vec))
+    }
+
+    /// Clear the CACHE.
+    ///
+    /// Return HashMap pointer.
+    ///
+    /// (fn CACHE)
+    #[defun]
+    fn clear_cache(cache: String) -> Result<()> {
+        Ok(())
+    }
+
     Ok(())
 }
